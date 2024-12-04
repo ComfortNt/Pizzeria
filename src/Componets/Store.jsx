1878cfa164a3aeb2b1c41a92b1fcbea60afbf41a
ComfortNt <128999583+ComfortNt@users.noreply.github.com>
2024-12-04 09:24:57 +0200

    Finished
    Routes done

--- src/Componets/Store.jsx
+++ src/Componets/Store.jsx
@@ -7,27 +7,28 @@ function Store(){
 
     return(
         
-        <div className="store">
+        <div className="store"> 
 
-             <div className="store-grid">
-                      {
-                       List.map( (item , key) =>{ return(
+           
 
-                  <div className="store-card">
-     
-                        <div className="store-image" style={{backgroundImage:`url(${item.Image})`}}>
-                        </div>
+              <div className="store-grid">
+                
+                        {
+                        List.map( (item , key) =>{ return(
 
-                        <div className="store-price">
-                          <h3>{item.name}</h3>
-                          <p>{item.price}</p>
-                        </div>
+                    <div className="store-card">
+      
+                          <div className="store-image" style={{backgroundImage:`url(${item.Image})`}}>
+                          </div>
 
-                        <button>Add to Cart</button>
-                        
-                  </div>
-                  )})}
-              </div>
+                          <div className="store-price">
+                            <h3>{item.name}</h3>
+                            <p>${item.price}</p>
+                          </div>
+                          <button>Add to Cart</button>
+                    </div>
+                    )})}
+                </div>
           </div>
 
     )
