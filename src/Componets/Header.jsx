bfb9249ae09411c6250bffaf5d60155cbb8ba76f
ComfortNt <128999583+ComfortNt@users.noreply.github.com>
2024-12-06 17:58:24 +0200

    fixing navbar
    ...

--- src/Componets/Header.jsx
+++ src/Componets/Header.jsx
@@ -1,25 +1,38 @@
 import { Link } from 'react-router-dom';
 import '../Styles/Header.css'
 import Image from '../assets/pizzaLogo.png'
+import { useRef } from 'react';
+import {FaTimes, FaBars} from 'react-icons/fa'
 
 function Header(){
+
+
+
+    const styleRef = useRef();
+
+
+    const navbar =()=>{
+        styleRef.current.classList.toggle("responsive-nav");
+    }
 
 
     return(
         
         <div className="navbar">
 
-                <div className="navbar_logo">
+                <div className="navbar_logo">  
                     <img src={Image} alt="Pizza-Image" />
                 </div>
 
-                <div className="nabvar_links">
+                <nav ref={styleRef}>
                     <Link to='/'>Home</Link> 
                     <Link to='/Store'>Store</Link>
                     <Link to='/About'>About</Link>
                     <Link to='/Contact'>Contact</Link>
-                </div>
+                    <button onClick={ ()=> navbar()}><FaTimes/></button>
+                </nav>
 
+                <button onClick={ ()=> navbar()}><FaBars/></button>
             
         </div>
     )
